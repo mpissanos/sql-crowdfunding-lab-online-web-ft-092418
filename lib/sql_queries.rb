b52eab8bd1dd0ba5252a0a3d4485f127f41455e6
Manny Pissanos <mpissano@gmail.com>
2018-10-24 17:04:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,8 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT users.name, SUM(pledges.amount) as total
-FROM pledges ON users.id"
+FROM pledges JOIN pledges ON users.id = pledges.user_id
+ORDER BY users.total;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
