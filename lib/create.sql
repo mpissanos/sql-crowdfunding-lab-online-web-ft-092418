fcd66bd8b17a74b52c75c61809f4e942e9d729c2
Manny Pissanos <mpissano@gmail.com>
2018-10-24 17:15:09 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  category TEXT,
+  funding_goal TEXT,
+  start_date TEXT,
+  end_date TEXT
+) ;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  age INTEGER
+) ;
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  amount INTEGER,
+  project_id INTEGER,
+  user_id INTEGER
+) ;
