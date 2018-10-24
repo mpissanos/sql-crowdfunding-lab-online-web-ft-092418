d4280aea30e9ab19da1718c1e51fd0d4f769382f
Manny Pissanos <mpissano@gmail.com>
2018-10-24 16:57:10 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,11 +21,10 @@ ORDER BY name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT title, SUM(pledges.amount) - funding_goal as overage 
+"SELECT projects.title, SUM(pledges.amount) - funding_goal as overage 
 FROM projects JOIN pledges ON projects.id= pledges.project_id
-GROUP BY project.title;"
-
-end
+GROUP BY project.title
+WHERE overage >= 0;"
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "Write your SQL query Here"
